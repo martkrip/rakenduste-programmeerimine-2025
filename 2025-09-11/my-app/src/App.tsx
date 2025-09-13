225492fff7b50243a66d25d0afe18d8562f54434
martintk <martintk@tlu.ee>
2025-09-13 14:30:27 +0300

    feat(profile): lisasin profiili komponendi: H1 element minu nimega, list huvidest, kontaktivorm e-meili ja tekstväljaga, Call to Action nupp

--- 2025-09-11/my-app/src/App.tsx
+++ 2025-09-11/my-app/src/App.tsx
@@ -1,7 +1,13 @@
 import Counter from "./components/Counter"
+import Profile from "./components/Profile"
 
 function App() {
-  return <Counter />
+  return (
+  <>
+  <Profile />
+  <Counter />
+  </>
+  )
 }
 
 export default App
---
+++ 2025-09-11/my-app/src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Profile.css"
+function Profile() {
+  return (
+    <div className="profile-container">
+    <h1>Martin Toomas Kripsaar </h1>
+    <ul>
+        <li>Lemmikmängud: Final Fantasy XIV, Deltarune, Undertale, Hearts of Iron 4, Celeste.</li>
+        <li>Sport: Jooksmine, jalgratta sõitmine ja jõusaal</li>
+        <li>Praegune mäng mida mängin: Final Fantasy XIV</li>
+        <li>Praegune anime mida vaatan: Bocchi the Rock!</li>
+    </ul>
+    E-meil: <input type='email' />
+    Tekstiväli: <textarea></textarea>
+    <button>nupp </button>
+    </div>
+  )
+}
+
+export default Profile
