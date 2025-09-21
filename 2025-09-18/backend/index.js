387354a612ba163fa77a3b87e0531c76524f09a3
martintk <martintk@tlu.ee>
2025-09-21 16:27:04 +0300

    (feat)Lisasin backendi funktsionaalsust (GET;POST;PUT;DELETE) kui ka mõningaid näiteid (flights, books). Testisin Postmaniga

--- 2025-09-18/backend/index.js
+++ 2025-09-18/backend/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
   res.send('Hello Martin!')
+})
+app.get("/users/:id", (req, res) => {
+  res.send(req.params)
+})
+
+app.get("/users/:userId/books/:bookId", (req, res) => {
+  res.send(req.params)
+})
+app.get("/flights/:from-:to", (req, res) => {
+  res.send(req.params)
+})
+
+app.post("/users", (req, res) => {
+  res.send("Got a POST request")
+})
+app.put("/users/:id", (req, res) => {
+  res.send(req.params)
+})
+
+app.delete("/users/:id", (req, res) => {
+  res.send(req.params)
 })
 
 app.listen(port, () => {
