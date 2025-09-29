4de33b9ea4ee41a411e3ce48e4b9927eb9189347
martintk <martintk@tlu.ee>
2025-09-29 20:39:21 +0300

    (feat) added adminTodos page

---
+++ 2025-09-25/martin/todo/todofrontend/src/components/AdminTodos.tsx
@@ -0,0 +1,166 @@
+import {
+  Box,
+  List,
+  ListItem,
+  Typography,
+  Button,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import SubmitTodo from "./SubmitTodo";
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: number;
+  updatedAt: number | null;
+  deleted: boolean;
+};
+
+const AdminTodos = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const fetchTodos = async () => {
+    const response = await fetch("http://localhost:3000/admin/todos");
+    const data = await response.json();
+
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h1">Admin: ToDoList</Typography>
+      <TodosList todos={todos} fetchTodos={fetchTodos} />
+      <SubmitTodo fetchTodos={fetchTodos} />
+    </Box>
+  );
+};
+
+type TodosListProps = {
+  todos: Todo[];
+  fetchTodos: () => void;
+};
+
+const TodosList: React.FC<TodosListProps> = ({ todos, fetchTodos }) => {
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [newTitle, setNewTitle] = useState("");
+  console.log({ todos });
+  const toggleDeleteHandler = async (id: string) => {
+    try {
+      await fetch("http://localhost:3000/admin/todos/toggle-delete", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
+      fetchTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const updateHandler = async (id: string) => {
+    try {
+      if (!newTitle) return;
+      await fetch("http://localhost:3000/todos", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, title: newTitle }),
+      });
+      setEditingId(null);
+      setNewTitle("");
+      fetchTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const completeHandler = async (id: string) => {
+    try {
+      await fetch("http://localhost:3000/todos", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, completed: true }),
+      });
+      fetchTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <List>
+      {todos.map((todo) => (
+        <ListItem key={todo.id}>
+          {editingId === todo.id ? (
+            <>
+              <TextField
+                size="small"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => updateHandler(todo.id)}
+              >
+                Save
+              </Button>
+              <Button variant="outlined" onClick={() => setEditingId(null)}>
+                Cancel
+              </Button>
+            </>
+          ) : (
+            <>
+              <Typography
+                sx={{
+                  flexGrow: 1,
+                  textDecoration: todo.completed ? "line-through" : "none",
+                }}
+              >
+                {todo.title}
+              </Typography>
+              {!todo.completed && (
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => completeHandler(todo.id)}
+                >
+                  Complete
+                </Button>
+              )}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setEditingId(todo.id);
+                  setNewTitle(todo.title);
+                }}
+              >
+                {" "}
+                Update
+              </Button>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => toggleDeleteHandler(todo.id)}
+              >
+                {" "}
+                {todo.deleted ? "Undelete" : "Delete"}
+              </Button>
+            </>
+          )}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default AdminTodos;
