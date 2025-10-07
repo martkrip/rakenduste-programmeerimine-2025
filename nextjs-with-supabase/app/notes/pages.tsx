e97ef8ca59cd31c81d9972ae072dd486cf1b7751
Martkrip <martintk@tlu.ee>
2025-10-07 14:27:26 +0300

    (feat) added notes to page.tsx

--- nextjs-with-supabase/app/notes/pages.tsx
+++ nextjs-with-supabase/app/notes/pages.tsx
@@ -1,8 +1,29 @@
 import { createClient } from "@/lib/supabase/server";
-
+import NoteItem from "./NoteItem";
 export default async function Page() {
   const supabase = await createClient();
   const { data: notes } = await supabase.from("notes").select();
+return (
+  <div className="max-w-3x1 mx-auto p-6">
+    <h1 className="'text-2x1 font-bold mb-4">Notes</h1>
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+    <div className="mb-6">
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="New note..."
+          className="flex-1 border rounded-1g px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+        />
+        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-1g">
+        Add
+        </button>
+      </div>
+    </div>
+    <div className="space-y-4">
+      {notes.map((note) => (
+        <NoteItem key={note.id} note={note} />
+      ))}
+      </div>
+  </div>
+)
 }
